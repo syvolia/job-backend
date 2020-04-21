f3d3678b712ab9c26036c6408b21c5de5e798990
syvolia <syvoliamary@gmail.com>
2020-04-22 02:02:10 +0300

    fixing .gitignore

--- worker/index.js
+++
@@ -1,6 +0,0 @@
-var CronJob = require('cron').CronJob;
-
-const fetchGithub = require('./tasks/fetch-github')
-
-// fetch github jobs
-new CronJob('* * * * *', fetchGithub, null, true, 'America/Los_Angeles');
